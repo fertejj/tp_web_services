918bb4b6571981c16e2e4d435c2d64934439943c
Fernando Javier Tejerina <fertejerina97@gmail.com>
2024-05-21 16:00:11 -0300

    create autos service y component

--- src/app/components/autos/autos.component.ts
+++ src/app/components/autos/autos.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { AutosService } from '../../services/autos/autos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-autos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './autos.component.html',
-  styleUrl: './autos.component.css'
+  styleUrl: './autos.component.css',
 })
 export class AutosComponent {
+  // me devuelve un array con 259 objetos cada objeto tiene un id y name
+  constructor(private autosService: AutosService) {}
 
+  makes: any[] = [];
+  getMakes() {
+    return this.autosService.getMakes().subscribe(
+      (response) => {
+        console.log(response);
+        this.makes = response;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getModels(){
+    return this.autosService.getModels().subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/services/autos/autos.service.ts
+++ src/app/services/autos/autos.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutosService {
+  constructor(private _http: HttpClient) {}
+  
+  public getMakes(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-RapidAPI-Key': '474f0f4b4amshc639a3fb3856c2fp195014jsnb949f3f5bd98',
+        'X-RapidAPI-Host': 'car-specs.p.rapidapi.com'
+      }),
+    };
+    return this._http.get('https://car-specs.p.rapidapi.com/v2/cars/makes', httpOptions);
+  }
 
-  constructor() { }
+  public getModels(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'X-RapidAPI-Key': '474f0f4b4amshc639a3fb3856c2fp195014jsnb949f3f5bd98',
+        'X-RapidAPI-Host': 'car-specs.p.rapidapi.com'
+      }),
+    };
+    return this._http.get('https://car-specs.p.rapidapi.com/v2/cars/makes/%7BmakeId%7D/models', httpOptions);
+  }
 }
