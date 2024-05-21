9029a4a2331776e5a1e35fc419958b4035dc5bdb
Fernando Javier Tejerina <fertejerina97@gmail.com>
2024-05-21 01:58:27 -0300

    update

--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -7,29 +7,30 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './noticias.component.html',
-  styleUrl: './noticias.component.css'
+  styleUrl: './noticias.component.css',
 })
 export class NoticiasComponent {
   noticias: any[] = [];
 
-  constructor(private noticiasService: NoticiasService) { }
+  constructor(private noticiasService: NoticiasService) {}
 
   getNoticias() {
     return this.noticiasService.getNoticias().subscribe(
       (data) => {
-        
         console.log(data);
         for (let i = 0; i < 3; i++) {
-        this.noticias[i] = data.homepageArticles[0].articles[i]}
+          this.noticias[i] = data.homepageArticles[0].articles[i];
+        }
         console.log(this.noticias);
       },
       (err) => {
         console.log(err);
       }
-    )
+    );
   }
   ngOnInit(): void {
     this.getNoticias();
   }
-  
+
+
 }
--- src/app/services/binance/binance.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class BinanceService {
-
-  constructor() { }
-}
